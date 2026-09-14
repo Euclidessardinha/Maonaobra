2b9e4d112c116b7c510b800a3516f26cf5545be7
Euclides Sardinha <euclidessardinha841@gmail.com>
2026-09-14 16:07:53 -0700

    r

--- backend/app/main.py
+++ backend/app/main.py
@@ -85,7 +85,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://maonaobra-nine.vercel.app",
+        "https://maonaobraa.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
